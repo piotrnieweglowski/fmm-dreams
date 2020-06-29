89789dfb38fec9d44d6cfde7371258aff5e07697
Piotr Niewęgłowski <piotr.nieweglowski@yahoo.pl>
2020-06-29 21:55:12 +0200

    Add Insert, Update, Delete

--- FMM/Features/DreamController.cs
+++ FMM/Features/DreamController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FMM.Controllers
@@ -23,6 +24,39 @@ namespace FMM.Controllers
         public ActionResult GetAll()
         {
             return Ok(Dreams);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(Guid Id)
+        {
+            return Ok(Dreams.FirstOrDefault(x => x.Id == Id));
+        }
+
+        [HttpPost]
+        public ActionResult Post(Dream dream)
+        {
+            Dreams.Add(dream);
+
+            return NoContent();
+        }
+
+        [HttpPut]
+        public ActionResult Put(Dream dream)
+        {
+            var toUpdate = Dreams.First(x => x.Id == dream.Id);
+            toUpdate.Title = dream.Title;
+            toUpdate.Description = dream.Description;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(Guid id)
+        {
+            var toRemove = Dreams.First(x => x.Id == id);
+            Dreams.Remove(toRemove);
+
+            return NoContent();
         }
     }
 }
