7c5bc7c6ec8026bddb78d951aa5d0599cbc807c7
Piotr Niewęgłowski <piotr.nieweglowski@yahoo.pl>
2020-06-29 20:56:25 +0200

    Initial commit

---
+++ FMM/Features/DreamController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FMM.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DreamController : ControllerBase
+    {
+        private static readonly IList<Dream> Dreams = new List<Dream>
+        {
+            new Dream { Id = Guid.NewGuid(), Title = "First Dream" },
+            new Dream { Id = Guid.NewGuid(), Title = "Second Dream" }
+        };
+
+
+        public DreamController()
+        {
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            return Ok(Dreams);
+        }
+    }
+}
