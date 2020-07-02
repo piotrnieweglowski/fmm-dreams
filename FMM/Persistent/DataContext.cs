805d05dcea3be5723d5ce080cfdfa9b68be85bfa
Piotr Niewęgłowski <piotr.nieweglowski@yahoo.pl>
2020-07-02 22:08:00 +0200

    Add initial DB

---
+++ FMM/Persistent/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace FMM.Persistent
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Dream> Dreams { get; set; }
+    }
+}
---
+++ FMM/Persistent/Dream.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FMM.Persistent
+{
+    public class Dream
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
