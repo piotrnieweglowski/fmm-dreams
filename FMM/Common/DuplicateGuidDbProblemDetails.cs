c691b9b8ae0d912b31ae15cde7fbe596cf00f1f7
Dominika <dominikasaide@gmail.com>
2020-07-11 22:45:41 +0200

    Add Step & Category Validators, Error handling Added Step & Category Validators, Not Found Exception Handling and DbUpdateException handling

---
+++ FMM/Common/DuplicateGuidDbProblemDetails.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FMM.Common
+{
+    public class DuplicateGuidDbProblemDetails : ProblemDetails
+    {
+        public DuplicateGuidDbProblemDetails(DbUpdateException exception)
+        {
+            Title = exception.Message;
+            Status = StatusCodes.Status400BadRequest;
+            Detail = exception.Source;
+            Type = $"https://httpstatuses.com/{Status}";
+        }
+    }
+}
