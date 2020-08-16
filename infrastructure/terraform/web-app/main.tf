6dd2ebe3d4288afe16cf15b1ace19a32c4991cf1
Rafal Turel <rafal.turel@gmail.com>
2020-08-16 18:01:08 +0200

    terraform added web app module

---
+++ infrastructure/terraform/web-app/main.tf
@@ -0,0 +1,13 @@
+provider "azurerm"{
+    version = "2.19.0"
+    subscription_id = "ff581936-424c-495e-a9ed-0c247bdd4d78"
+    features {}
+}
+
+module "web-app-container" {
+  source  = "./terraform-azurerm-web-app-container"
+  resource_group_name = "fmmResourceGroup"
+  name  = "fmm"
+  container_type = "docker"
+  container_image = "fmmContainerRegistry/fmmapi"
+}
