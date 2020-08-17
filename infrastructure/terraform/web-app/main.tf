76306821bbd49d4a89bd44fa4f8741cbcdc60109
Rafal Turel <rafal.turel@gmail.com>
2020-08-17 08:00:19 +0200

    updates for web app

--- infrastructure/terraform/web-app/main.tf
+++ infrastructure/terraform/web-app/main.tf
@@ -9,11 +9,15 @@ module "web-app-container" {
   resource_group_name = "DefaultResourceGroup-WEU"
   name  = "fmm"
   container_type = "docker"
-  container_image = "fmmBase/fmmapi"
+  container_image = "fmmbase/fmmapi"
   plan = {
     sku_size  = "B1"
   }
   docker_registry_url = "fmmbase.azurecr.io"
   docker_registry_username = var.cr_user
   docker_registry_password = var.cr_pass
+
+  app_settings = {
+    DbConnection = "Server=fmm-postgresql-server-1.postgres.database.azure.com;Database=fmm-dreams;Port=5432;User Id=turelit@fmm-postgresql-server-1;Password=${var.db_pass};Ssl Mode=Require;"
+  }
 }
