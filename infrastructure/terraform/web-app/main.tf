5e920b7b49eb2de619309b9de17082892973e9db
Rafal Turel <rafal.turel@gmail.com>
2020-08-17 07:36:50 +0200

    split web-app

--- infrastructure/terraform/web-app/main.tf
+++ infrastructure/terraform/web-app/main.tf
@@ -3,20 +3,17 @@ provider "azurerm"{
     subscription_id = "ff581936-424c-495e-a9ed-0c247bdd4d78"
     features {}
 }
-module "container-registry"{
-  source = "./container-registry"
-}
 
 module "web-app-container" {
   source  = "./terraform-azurerm-web-app-container"
-  resource_group_name = "fmmResourceGroup"
+  resource_group_name = "DefaultResourceGroup-WEU"
   name  = "fmm"
   container_type = "docker"
-  container_image = "fmmContainerRegistry/fmmapi"
+  container_image = "fmmBase/fmmapi"
   plan = {
     sku_size  = "B1"
   }
-  docker_registry_url = "fmmcontainerregistry.azurecr.io"
-  docker_registry_username = "${data.azurerm_container_registry.fmm-container-registry.admin_username}"
-  docker_registry_password = "${data.azurerm_container_registry.fmm-container-registry.admin_password}"
+  docker_registry_url = "fmmbase.azurecr.io"
+  docker_registry_username = var.cr_user
+  docker_registry_password = var.cr_pass
 }
---
+++ infrastructure/terraform/web-app/variables.tf
@@ -0,0 +1,8 @@
+variable "cr_user"{
+  type              = string
+  description       = "Login for container registry"
+}
+variable "cr_pass"{
+  type              = string
+  description       = "Pass for container registry"
+}
