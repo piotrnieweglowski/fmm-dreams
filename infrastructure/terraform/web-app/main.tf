425c804c1fe6b3cdb72bf4780c3fd28f62fdf98d
Rafal Turel <rafal.turel@gmail.com>
2020-08-23 16:25:03 +0200

    fixed deploy job

--- infrastructure/terraform/web-app/main.tf
+++ infrastructure/terraform/web-app/main.tf
@@ -8,8 +8,8 @@ module "web-app-container" {
     sku_size  = "B1"
   }
   docker_registry_url = "http://fmmbase.azurecr.io"
- # docker_registry_username = var.cr_user
- # docker_registry_password = var.cr_pass
+  docker_registry_username = var.cr_user
+  docker_registry_password = var.cr_pass
 
   app_settings = {
     DbConnection = "Server=fmm-postgresql-server-1.postgres.database.azure.com;Database=fmm-dreams;Port=5432;User Id=turelit@fmm-postgresql-server-1;Password=${var.db_pass};Ssl Mode=Require;"
