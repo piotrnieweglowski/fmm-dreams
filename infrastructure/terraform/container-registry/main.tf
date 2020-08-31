2a48d24e629cf151da113849f19d78bfbdc6f9af
Rafal Turel <rafal.turel@gmail.com>
2020-08-31 06:55:37 +0200

    add explicit dependency in cr create

--- infrastructure/terraform/container-registry/main.tf
+++ infrastructure/terraform/container-registry/main.tf
@@ -9,6 +9,7 @@ resource "azurerm_key_vault_secret" "cr-user" {
   name         = "cr-user"
   value        = azurerm_container_registry.fmm-container-registry.admin_username
   key_vault_id = "/subscriptions/ff581936-424c-495e-a9ed-0c247bdd4d78/resourceGroups/DefaultResourceGroup-WEU/providers/Microsoft.KeyVault/vaults/fmm-dev"
+  depends_on = [azurerm_container_registry.fmm-container-registry]
 
   tags = {
     environment = "dev"
@@ -18,6 +19,7 @@ resource "azurerm_key_vault_secret" "cr-pass" {
   name         = "cr-pass"
   value        = azurerm_container_registry.fmm-container-registry.admin_password
   key_vault_id = "/subscriptions/ff581936-424c-495e-a9ed-0c247bdd4d78/resourceGroups/DefaultResourceGroup-WEU/providers/Microsoft.KeyVault/vaults/fmm-dev"
+  depends_on = [azurerm_container_registry.fmm-container-registry]
 
   tags = {
     environment = "dev"
