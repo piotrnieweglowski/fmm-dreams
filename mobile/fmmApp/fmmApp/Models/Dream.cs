6e976070063c49270b807a1062f69849cea74014
Piotr Nieweglowski <piotr.nieweglowski@yahoo.pl>
2020-09-02 07:40:57 +0200

    Add integration with API

---
+++ mobile/fmmApp/fmmApp/Models/Dream.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace fmmApp.Models
+{
+    [ResourcePath("dream")]
+    public class Dream : ResourceItem<Dream>
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ mobile/fmmApp/fmmApp/Models/ResourceItem.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Reflection;
+
+namespace fmmApp.Models
+{
+    public abstract class ResourceItem<T>
+    {
+        public string Id { get; set; }
+
+        [JsonIgnore]
+        public static string ResourcePath
+        {
+            get
+            {
+                return typeof(T).GetCustomAttribute<ResourcePathAttribute>().Value;
+            }
+        }
+
+        [JsonIgnore]
+        public string ResourcePathWithId
+        {
+            get
+            {
+                return CreatePathWithId(Id);
+            }
+        }
+
+        public static string CreatePathWithId(string id)
+        {
+            return $"{ResourcePath}/{id}";
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    sealed class ResourcePathAttribute : Attribute
+    {
+        public ResourcePathAttribute(string value)
+        {
+            Value = value;
+        }
+
+        public string Value { get; private set; }
+    }
+}
