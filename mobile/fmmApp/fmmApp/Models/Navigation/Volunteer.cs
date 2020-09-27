a3b1aea2ad9484649ecb80d41f46e8ded498e4d6
Dominika <dominikasaide@gmail.com>
2020-09-27 20:35:02 +0200

    Added missing Voulnteer class

---
+++ mobile/fmmApp/fmmApp/Models/Navigation/Volunteer.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.Serialization;
+using Xamarin.Forms.Internals;
+
+namespace fmmApp.Models.Navigation
+{
+    /// <summary>
+    /// Model for my address page.
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [DataContract]
+    public class Volunteer
+    {
+        #region Properties
+        /// <summary>
+        /// Gets or sets user name.
+        /// </summary>
+        [DataMember(Name = "firstName")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the address type.
+        /// </summary>
+        public string Department { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location.
+        /// </summary>
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the contact number.
+        /// </summary>
+        public string Email { get; set; }
+
+        #endregion
+    }
+}
